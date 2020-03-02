c70c3ff1389ff329a283a319385931e024529a2f
Sarah <sarahlohmes@gmail.com>
2020-03-02 11:10:47 -0800

    create new chirps and insert them into database

--- app.js
+++ app.js
@@ -3,25 +3,19 @@
 const express = require('express');
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'chirpsAdmin',
-	password: 'dataBird',
-	database: 'chirps'
-});
-
-connection.connect();
-
 let app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
+app.use(express.urlencoded({ extended: true }));
 
 // index page
+const getChirpsSql = 'SELECT * FROM chirps';
+
 app.get('/index', (req, res) => {
 	let chirps = [];
-	connection.query('SELECT * FROM chirps', (err, rows) => {
+	connection.query(getChirpsSql, (err, rows) => {
 	  if (err) throw err;
 		rows.forEach((row) => {
 			chirps.push({
@@ -31,7 +25,28 @@ app.get('/index', (req, res) => {
 		});
 		res.render('index', {'chirps': chirps});
 	});
+});
 
+app.post('/create', (req, res) => {
+	const newChirp = {text: req.body.text}; // this user input should be escaped, for safety
+	connection.query('INSERT INTO chirps SET ?', newChirp, (err, conRes) => {
+	  if (err) throw err;
+	  console.log('Inserted new chirp with ID:', conRes.insertId);
+
+		let chirps = [];
+		connection.query(getChirpsSql, (err, rows) => {
+		  if (err) throw err;
+			rows.forEach((row) => {
+				chirps.push({
+		      'id': row.id,
+		      'text': row.text
+		    });
+			});
+			res.render('index', {'chirps': chirps});
+		});
+	});
+
+	// TODO post to notification API
 });
 
 // 404
@@ -49,6 +64,17 @@ app.use((err, req, res, next) => {
 	res.send('500 - Server Error');
 });
 
-app.listen(app.get('port'), () => {
-	console.log('App is running at http://localhost:' + app.get('port') + '/index; press Ctrl + c to terminate.');
+let connection = mysql.createConnection({
+	host: 'localhost',
+	// ideally, these would be set via env vars...
+	user: 'chirpsAdmin',
+	password: 'dataBird',
+	database: 'chirps'
+});
+
+connection.connect(err => {
+	if (err) throw err;
+	app.listen(app.get('port'), () => {
+		console.log('App is running at http://localhost:' + app.get('port') + '/index; press Ctrl + c to terminate.');
+	});
 });
